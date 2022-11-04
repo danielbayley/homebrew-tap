64950397048d35bfd23c3e989b1b45faedc444b2
Daniel Bayley <daniel.bayley@me.com>
2022-11-04 22:07:52 +0000

    Update testy.rb

--- Casks/testy.rb
+++ Casks/testy.rb
@@ -1,12 +1,14 @@
 cask "testy" do
   version :latest
   sha256 :no_check
-  
+
+  owner = ENV.fetch "TOP_SECRET", nil
+
   branch = "main"
-  url "https://github.com/danielbayley/homebrew-tap/archive/refs/heads/#{branch}.zip"
+  url "https://github.com/#{owner}/homebrew-tap/archive/refs/heads/#{branch}.zip"
   name "Testy"
   desc "Testing"
-  homepage "https://github.com/danielbayley/homebrew-tap"
+  homepage "https://github.com/#{owner}/homebrew-tap"
 
   stage_only true
 end
