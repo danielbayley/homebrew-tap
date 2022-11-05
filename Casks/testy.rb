04c6175c62a6b009c93eb4601e1dd932a34cb1e5
Daniel Bayley <daniel.bayley@me.com>
2022-11-05 21:39:14 +0000

    Update testy.rb

--- Casks/testy.rb
+++ Casks/testy.rb
@@ -2,7 +2,7 @@ cask "testy" do
   version :latest
   sha256 :no_check
 
-  owner = ENV["HOMEBREW_TOP_SECRET"]
+  owner = ENV["HOMEBREW_TOP_SECRET"] # comment
   p "owner: #{owner}"
 
   branch = "main"
