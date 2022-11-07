24e46f62ecb05bfa97eeb1ea3103565c699152f8
Daniel Bayley <daniel.bayley@me.com>
2022-11-07 10:42:02 +0000

    Update testy.rb

--- Casks/testy.rb
+++ Casks/testy.rb
@@ -1,8 +1,10 @@
 cask "testy" do
   version :latest
   sha256 :no_check
-  
-  pp ENV.filter { |k,| k =~ /^HOMEBREW-(A|B|C)/ } # comment
+
+  p ENV.fetch "HOMEBREW_A"
+  p ENV.fetch "HOMEBREW_B"
+  p ENV.fetch "HOMEBREW_C"
 
   branch = "main"
   url "https://github.com/#{owner}/homebrew-tap/archive/refs/heads/#{branch}.zip"
