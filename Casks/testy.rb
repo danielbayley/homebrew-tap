6b2397a2f3b52c5050fcb3c4d7bab00d8142733a
Daniel Bayley <daniel.bayley@me.com>
2022-11-07 11:26:30 +0000

    Update testy.rb

--- Casks/testy.rb
+++ Casks/testy.rb
@@ -2,9 +2,9 @@ cask "testy" do
   version :latest
   sha256 :no_check
 
-  p ENV.fetch "HOMEBREW_A"
-  p ENV.fetch "HOMEBREW_B"
-  p ENV.fetch "HOMEBREW_C"
+  p "HOMEBREW_A: #{ENV.fetch 'HOMEBREW_A'}"
+  p "HOMEBREW_B: #{ENV.fetch 'HOMEBREW_B'}"
+  p "HOMEBREW_C: #{ENV.fetch 'HOMEBREW_C'}"
 
   owner = ENV.fetch "GITHUB_REPOSITORY_OWNER"
   branch = "main"
