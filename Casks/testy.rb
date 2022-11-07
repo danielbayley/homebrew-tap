09fd322c47db342cc98469c02fb0ac0e70f01da8
Daniel Bayley <daniel.bayley@me.com>
2022-11-07 11:12:31 +0000

    Update testy.rb

--- Casks/testy.rb
+++ Casks/testy.rb
@@ -1,16 +1,18 @@
 cask "testy" do
   version :latest
   sha256 :no_check
+  
+  owner = ENV.fetch "GITHUB_REPOSITORY_OWNER"
 
   p ENV.fetch "HOMEBREW_A"
   p ENV.fetch "HOMEBREW_B"
   p ENV.fetch "HOMEBREW_C"
 
   branch = "main"
-  url "https://github.com/#{GITHUB_REPOSITORY_OWNER}/homebrew-tap/archive/refs/heads/#{branch}.zip"
+  url "https://github.com/#{owner}/homebrew-tap/archive/refs/heads/#{branch}.zip"
   name "Testy"
   desc "Testing"
-  homepage "https://github.com/#{GITHUB_REPOSITORY_OWNER}/homebrew-tap"
+  homepage "https://github.com/#{owner}/homebrew-tap"
 
   stage_only true
 end
