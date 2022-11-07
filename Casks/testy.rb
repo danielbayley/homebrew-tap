b4e1e041685dada1430018847f1c3dc18152ec5d
Daniel Bayley <daniel.bayley@me.com>
2022-11-07 10:34:42 +0000

    Update testy.rb

--- Casks/testy.rb
+++ Casks/testy.rb
@@ -2,7 +2,7 @@ cask "testy" do
   version :latest
   sha256 :no_check
   
-  pp ENV.filter { |k,| k =~ /^HOMEBREW-(A|B|C)/ }
+  pp ENV.filter { |k,| k =~ /^HOMEBREW-(A|B|C)/ } # comment
 
   branch = "main"
   url "https://github.com/#{owner}/homebrew-tap/archive/refs/heads/#{branch}.zip"
