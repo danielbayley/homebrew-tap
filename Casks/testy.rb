8b612083de7b0fdd94a2bb41c3bdb23cfdbdf6e4
Daniel Bayley <daniel.bayley@me.com>
2022-11-09 14:42:23 +0000

    Update testy.rb

--- Casks/testy.rb
+++ Casks/testy.rb
@@ -2,7 +2,7 @@ cask "testy" do
   version :latest
   sha256 :no_check
 
-  p "HOMEBREW_USER:", ENV["HOMEBREW_USER"] || "FAILED" #comment
+  p "HOMEBREW_USER:", ENV["HOMEBREW_USER"] || "FAILED" # comment
 
   owner = ENV.fetch "GITHUB_REPOSITORY_OWNER"
   branch = "main"
